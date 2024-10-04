e6250fb9f9db92c4bd0bed8fda075d2de0efa556
Derek Johnston <101991363+usethisname1419@users.noreply.github.com>
2024-10-04 08:36:32 -0500

    Update HFSKv5.py

--- HFSKv5.py
+++ HFSKv5.py
@@ -732,10 +732,10 @@ if __name__ == '__main__':
                     next_pause_time = time.time() + pause_after_minutes * 60  # 
                 if args.sessions:
                     
-                    if len(users) > 1 and idx == 0:  # First user in list case
+                    if len(users) > 1:  # First user in list case
                         print(f"{Fore.WHITE}[{Fore.YELLOW}INFO{Fore.WHITE}]{Fore.RESET} Creating a new session..")
                         session = create_new_session()  # Your session creation logic here
-                    elif len(users) == 1 and idx % 12 == 0 and idx > 0:  # Single user case
+                    elif len(users) == 1 and i % 12 == 0::  # Single user case
                         print(f"{Fore.WHITE}[{Fore.YELLOW}INFO{Fore.WHITE}]{Fore.RESET} Creating a new session..")
                         session = create_new_session()  # Your session creation logic here
                 proxy_ip, proxy_port = next(proxy_gen) if proxy_gen else (None, None)
